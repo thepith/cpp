ccb4360745b7257d6317e6bf5be21a264345a390
Pascal Hebbeker <pascal.hebbeker@rwth-aachen.de>
2017-03-23 21:28:21 +0100

    implemented and tested function to calculate pentecost monday

---
+++ blatt-01/Pentecost.cpp
@@ -0,0 +1,20 @@
+// Copyright 2017, Pascal Hebbeker
+// Calculate the Pentacost date according to the gauss formula.
+// see https://de.wikipedia.org/wiki/Gau%C3%9Fsche_Osterformel
+// Returns the number days after the 30.th of April. Takes a year as an input.
+int DayOfPentecost(int year) {
+  int K = year/100;
+  int M = 15 + (3*K + 3)/4 - (8*K +12)/25;
+  int S = 2 - (3*K + 3)/4;
+  int A = year % 19;
+  int D = (19*A + M) %30;
+  int R = (D + A/11)/29;
+  int OG = 21 + D - R;
+  int SZ = 7 - (year + year/4 + S)%7;
+  int OE = 7 - (OG - SZ) % 7;
+  int OS = OG + OE; // easter in days in march
+  int Pentecost = OS + 50; // Pentacost (Monday) is 50 days after easter
+  int Pentecost_May = Pentecost - 61; // convert days in march to days in may
+  return Pentecost_May;
+}
+
---
+++ blatt-01/PentecostTest.cpp
@@ -0,0 +1,17 @@
+// Copyright 2017, Pascal Hebbeker
+// Test the Calculation of the Pentecost Date
+
+#include <gtest/gtest.h>
+#include "./Pentecost.cpp"
+
+TEST(Pentecost, DayOfPentecost) {
+  ASSERT_EQ(16, DayOfPentecost(2016));
+  ASSERT_EQ(36, DayOfPentecost(2017));
+  ASSERT_EQ(21, DayOfPentecost(2018));
+  ASSERT_EQ(41, DayOfPentecost(2019));
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
